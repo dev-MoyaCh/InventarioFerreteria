e8b4d6bc568639551e3b8a419e13d42775b8b6a4
74127790-cmd <74127790@continental.edu.pe>
2026-06-13 17:22:56 -0500

    Excepciones version 1

--- ExcepcionesInventario.java
+++ ExcepcionesInventario.java
@@ -1,1 +1,11 @@
+public class ProductoNoEncontradoException extends RuntimeException {
+    public ProductoNoEncontradoException(String id) {
+        super("Producto no encontrado con ID: " + id);
+    }
+}
 
+public class StockInsuficienteException extends RuntimeException {
+    public StockInsuficienteException(String nombre, int disponible, int solicitado) {
+        super("Stock insuficiente para '" + nombre + "' (Disponible: " + disponible + ", Solicitado: " + solicitado + ")");
+    }
+}
