35ee5562b2778e99d92ecc9b04efd30742d398d7
Ronaldo <skri_into@hotmail.com>
2026-06-26 23:15:41 -0500

    Implementar fase GREEN para FizzBuzz

--- kata-fizzbuzz/FizzBuzz.java
+++ kata-fizzbuzz/FizzBuzz.java
@@ -2,6 +2,10 @@ public class FizzBuzz {
 
     public String convertir(int numero) {
 
+        if (numero == 15) {
+            return "FizzBuzz";
+        }
+
         if (numero == 3) {
             return "Fizz";
         }
