27860336f51f5ed2db2390e538a1e8bd6721ff5c
tm795476-cyber <tm795476@continental.edu.pe>
2026-06-27 15:13:19 -0500

    Agregar prueba de registrar ingreso

--- Inventariotest.java
+++ Inventariotest.java
@@ -2,6 +2,18 @@ public class InventarioTest {
 
     public static void main(String[] args) {
 
+        Inventario inventario = new Inventario();
+
+        inventario.registrarIngreso("P001", "Martillo", 10, 25.50);
+
+        Producto producto = inventario.buscarPorId("P001");
+
+        if (producto != null) {
+            System.out.println("Prueba 1 correcta");
+        } else {
+            System.out.println("Prueba 1 fallo");
+        }
+
     }
 
 }
